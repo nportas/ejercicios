1d48e086ffa17986b7dfa268136d84b5876cdc58
Nayla Portas <nayla.portas@grupoesfera.com.ar>
2017-06-06 16:49:24 -0300

    Primera versión de ejercicio Factura

---
+++ escuelita-java/src/main/test/ar/com/java/escuelita/modelo/FacturaTest.java
@@ -0,0 +1,33 @@
+package ar.com.java.escuelita.modelo;
+
+import org.junit.Test;
+
+/**
+ * Se pretende modelar una factura, la cual está formada por distintos ítems utilizados en la refacción de una casa.
+ * Existen los ítems que son materiales (ladrillo, arena y cemento) y los ítems que son servicios de personas (albañil, pintor)
+ * de los cuales se puede obtener su precio según cantidad unitaria o en kg para los materiales, y según hs trabajadas para las personas. 
+ * La factura puede calcular su total en cualquier momento.
+ * Implementar el ejemplo, haciendo que funcionen correctamente los siguientes tests.
+ *	
+ * @author nportas
+ *
+ */
+public class FacturaTest {
+	
+	@Test
+	public void comprobarTotalConDiezLadrillosYDosKilosDeArena() {
+		
+	}
+	
+	
+	@Test
+	public void comprobarTotalConCincoKilosDeCementoYAlbanil() {
+		
+	}
+	
+	@Test
+	public void comprobarTotalConUnKiloDeArenaPintorYAlbanil() {
+		
+	}
+
+}
